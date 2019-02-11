4832660919784a583732d1d639e11e71d362dfd2
Radovan-Drincic <drincic997>
2019-02-11 15:02:41 +0100

    Add hello.py file

--- hello.py
+++ hello.py
@@ -1,8 +1,15 @@
+korisnik_unio_prvi = input("Unesite_prvi_broj: ")
+korisnik_unio_drugi = input("Unesite_drugi_broj: ")
+if not korisnik_unio_prvi.isnumeric() or not korisnik_unio_drugi.isnumeric() :
+    print("Nijeste_unijeli_dobar_broj")
+    quit()
+print(korisnik_unio_prvi)
+print(korisnik_unio_drugi)
 
-def greet(ime,prezime):
-    print("Hello there " +  ime + " " + prezime + "!")
+broj_prvi = int(korisnik_unio_prvi)
+broj_drugi = int(korisnik_unio_drugi)
 
-greet("Radovan" , "Drincic")
-
-
-
+zbir= korisnik_unio_prvi + korisnik_unio_drugi
+print(zbir)
+print(int("5"))
+print(type("5"))
